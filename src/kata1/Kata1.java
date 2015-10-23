3c4b8766e40f9c2aac3708f98ccea0226f242817
Chaxi <Chaxi@192.168.1.13>
2015-10-23 11:58:22 +0100

    Tercera version

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,6 +1,7 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
@@ -8,12 +9,13 @@ public class Kata1 {
         System.out.println("Hola");
        
         Person p1;
-        Date f;
-        f = new Date(93,11,25);
+        Calendar f;
+        f = GregorianCalendar.getInstance();
+        f.set(1993, 11, 25);
         p1 = new Person("Chaxiraxi", "Cabrera", f);
         
         System.out.println(p1.getFullName());
-        System.out.println(p1.getBirthday());
+        System.out.println(p1.getBirthday().getTime());
         System.out.println(p1.getAge());
         System.out.println("Fin");
     }
